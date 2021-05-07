f1a675598170815e3344b6c94e6c0c6b2d17f168
Manthan Laad <laad.manthan@gmail.com>
2021-05-07 15:33:01 +0530

    Added JS and APIs

---
+++ app.js
@@ -0,0 +1,68 @@
+// API Keys and links
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=65646db72b3ea126da5a9f54222bde62&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w500";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=65646db72b3ea126da5a9f54222bde62&query="';
+
+// DOM Selections
+const form = document.getElementById("form");
+const main = document.getElementById("main");
+
+getMovies(API_URL);
+
+form.addEventListener("submit", (e) => {
+  // Prevents form Submission
+  e.preventDefault();
+
+  const searchTerm = search.value;
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
+
+// Fumctions
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = "";
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+      <img
+        src="${IMG_PATH + poster_path}"
+        alt="${title}"
+      />
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByVote(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview">
+        <h3>Overview</h3>
+        ${overview}
+      </div>
+      `;
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByVote(vote) {
+  if (vote >= 7.5) {
+    return "green";
+  } else if (vote >= 5) {
+    return "yellow";
+  } else if (vote >= 2.5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
